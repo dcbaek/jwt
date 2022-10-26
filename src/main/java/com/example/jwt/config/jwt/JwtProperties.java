e0adfa468b81d839b67e3ede6e42de752939b83f
dcbaek <ehdco8988@gmail.com>
2022-10-26 14:12:53 +0900

    removed cached

--- src/main/java/com/example/jwt/config/jwt/JwtProperties.java
+++ src/main/java/com/example/jwt/config/jwt/JwtProperties.java
@@ -2,7 +2,7 @@ package com.example.jwt.config.jwt;
 
 public interface JwtProperties {
     String SECRET = "hanait1010";
-    int EXPIRATION_TIME = 864000000;
+    int EXPIRATION_TIME = 600;
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
 }
