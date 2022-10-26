2626ca4bc4f7691a0a51a1cf6e3629e3b33a3625
dcbaek <ehdco8988@gmail.com>
2022-10-26 16:15:56 +0900

    gitignore add03

--- src/main/java/com/example/jwt/config/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.example.jwt.config.jwt;
-
-public interface JwtProperties {
-    String SECRET = "hanait1010";
-    int EXPIRATION_TIME = 600;
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-}
