7693008cb7fee9d3e104634acac9cb542083327a
dcbaek <ehdco8988@gmail.com>
2022-10-27 16:08:27 +0900

    Dto update

--- src/main/java/com/example/jwt/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/example/jwt/config/auth/PrincipalDetailsService.java
@@ -4,6 +4,7 @@ import com.example.jwt.model.User;
 import com.example.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
--- src/main/java/com/example/jwt/dto/LoginRequestDto.java
+++ src/main/java/com/example/jwt/dto/LoginRequestDto.java
@@ -6,4 +6,6 @@ import lombok.Data;
 public class LoginRequestDto {
     private String username;
     private String password;
+    private String birthDay;
+    private String sex;
 }
